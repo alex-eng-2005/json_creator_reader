0f50ed30be83dbb2767090d9d6f0316db282e1e8
alex-eng-2005 <staridoxberyl2005@gmail.com>
2026-01-06 18:10:06 -0800

    Added some stuff

---
+++ public/index.js
@@ -0,0 +1,14 @@
+//Create button 
+const create = document.getElementById("create_btn")
+//Read button
+const read = document.getElementById("read_btn")
+
+//Goes to the read html
+read.addEventListener("click", ()=>{
+    window.location.href ="reader.html"
+})
+
+//Goes to the create html
+create.addEventListener("click", ()=>{
+    window.location.href ="reader.html"
+})
