5d0396bb9ded7d8afb49573f7a93dcee8484e3f4
alex-eng-2005 <staridoxberyl2005@gmail.com>
2026-01-07 23:56:18 -0800

    Added new folders and organization

--- public/index.js
+++ public/index.js
@@ -5,10 +5,10 @@ const read = document.getElementById("read_btn")
 
 //Goes to the read html
 read.addEventListener("click", ()=>{
-    window.location.href ="reader.html"
+    window.location.href ="Reader/reader.html"
 })
 
 //Goes to the create html
 create.addEventListener("click", ()=>{
-    window.location.href ="creator.html"
+    window.location.href ="Creator/creator.html"
 })
