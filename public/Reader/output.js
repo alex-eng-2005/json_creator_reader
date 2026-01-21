6463fda49c04344cf4672fab9182e2a622ecf52f
alex-eng-2005 <staridoxberyl2005@gmail.com>
2026-01-21 14:40:58 -0800

    Version 1.5

--- public/Reader/output.js
+++ public/Reader/output.js
@@ -1,31 +1,76 @@
-//import {get_json, clean_json, decodeJSON} from "reader.js"
+
+//Reads the json
+function clean_json(data)
+{
+    return JSON.stringify(data)
+}
 
 //Reveals the entire sessions
 function reveal()
 {
+    //Gets our table
     let table = document.getElementById("results")
-    let obj = sessionStorage.getItem("key_values")
-    console.log(JSON.parse(obj))
-    for(let i = 0; i < 10; i++)
+    //Gets our object
+    let obj = JSON.parse(sessionStorage.getItem("key_values"))
+    //Index for buttons
+    let i = 0;
+    //Resets the session storage for safety reasions
+    sessionStorage.setItem("keyItems",null);
+    
+    //Loops through the object
+    for(let key in obj)
     {
+        //Gets the rows and table data
         let tr = document.createElement("tr")
         let td1 = document.createElement("td")
         let td2 = document.createElement("td")
         let td3 = document.createElement("td")
+        let td4 = document.createElement("td")
         
         //Session
-        td1.textContent = i
-        td2.textContent = "SILVER"
-        td3.textContent = i
-
-        tr.appendChild(td1)
-        tr.appendChild(td2)
-        tr.appendChild(td3)
+        td1.textContent = key
+        td2.textContent = typeof(obj[key])
         
-        table.appendChild(tr);
+        //Checks if td4 is a json
+        if(typeof(obj[key]) == "object")
+        {
+            //Button
+            let readObj = document.createElement("button")
+            readObj.setAttribute("id", "button" + i);
+            readObj.textContent = "Read This JSON"
+            
+            //Reads the button
+            td4.appendChild(readObj);
+            td3.textContent = JSON.stringify(obj[key])
+            //Adds everything
+            tr.appendChild(td1)
+            tr.appendChild(td2)
+            tr.appendChild(td3)
+            tr.appendChild(td4)
+        
+            table.appendChild(tr);
+            //Reads the button
+            document.getElementById("button" + i).addEventListener("click", ()=>{
+                console.log("button" + i);
+            });
+            i++;
+        }
+        else
+        {
+            td4.textContent = "Not a JSON"
+            td3.textContent = obj[key]
+            //Adds everything
+            tr.appendChild(td1)
+            tr.appendChild(td2)
+            tr.appendChild(td3)
+            tr.appendChild(td4)
+        
+            table.appendChild(tr);
+        }
+        
     }
 
 }
 
-//Reveals the JSON
+//Reveals the JSON once we enter the screen
 reveal();
